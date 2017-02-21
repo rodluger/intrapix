5a3eacc933b690a1fc8f368fee75dcc15399ef2c
Rodrigo Luger <rodluger@gmail.com>
2017-02-21 09:43:27 -0800

    mcmc

--- intrapix.py
+++ intrapix.py
@@ -4,12 +4,18 @@
 intrapix.py
 -----------
 
+In the future:
+
+  G = lambda y, x: Gauss2D(x, y, 5, 5, 0.2, 0.4, 0)
+  print(dblquad(G, 0, 10, lambda x: 0, lambda x: 10))
+
 '''
 
 from __future__ import division, print_function, absolute_import, unicode_literals
 import numpy as np
 import matplotlib.pyplot as pl
 import matplotlib.animation as animation
+from scipy.integrate import dblquad
 
 def Grid(nx, ny, res):
   '''
@@ -38,7 +44,8 @@ def Gauss2D(x, y, x0, y0, sx, sy, theta):
   a = ((np.cos(theta) ** 2) / (2 * sx ** 2)) + ((np.sin(theta) ** 2) / (2 * sy ** 2))
   b = -((np.sin(2 * theta)) / (4 * sx ** 2)) + ((np.sin(2 * theta)) / (4 * sy ** 2))
   c = ((np.sin(theta) ** 2) / (2 * sx ** 2)) + ((np.cos(theta) ** 2) / (2 * sy ** 2))
-  return np.exp(-(a * (x - x0) ** 2 + 2 * b * (x - x0) * (y - y0) + c * (y - y0) ** 2))
+  norm = 1. / (2 * np.pi * sx * sy)
+  return norm * np.exp(-(a * (x - x0) ** 2 + 2 * b * (x - x0) * (y - y0) + c * (y - y0) ** 2))
 
 def GaussMixture2D(X, Y, x0, y0, sx, sy, amp, theta):
   '''
@@ -61,7 +68,7 @@ def GaussMixture2D(X, Y, x0, y0, sx, sy, amp, theta):
   # Add the gaussians
   res = np.zeros_like(X)
   for i in range(nmix):
-    res += Gauss2D(X, Y, x0[i], y0[i], sx[i], sy[i], theta[i])
+    res += amp[i] * Gauss2D(X, Y, x0[i], y0[i], sx[i], sy[i], theta[i])
   return res
 
 def IPV(nx, ny, res, cx, cy):
@@ -70,6 +77,8 @@ def IPV(nx, ny, res, cx, cy):
   
   '''
   
+  NormalizeIPV(cx)
+  NormalizeIPV(cy)
   z = np.linspace(0, 1, res)
   ipv = np.ones((ny * res, nx * res))  
   for i in range(nx):
@@ -111,59 +120,136 @@ def Pixelate(prf, nx, ny, res):
       pix[i][j] = np.sum(prf[j * res:(j + 1) * res, i * res:(i + 1) * res])
   return pix.T
 
-def Animate(nx = 8, ny = 8, res = 100, sx = 0.75, sy = 0.5, amp = 1.,
-            theta = -0.3, cx = [0.75, 1, -1], cy = [0.75, 1, -1],
-            eps = 0.1, dx = 0.25, dy = 0.25):
+def NormalizeIPV(c):
+  '''
+  Sets the first IPV coefficient by requiring that the IPV integrate 
+  to 1 over the interval [0,1].
+  
+  '''
+  
+  c[0] = 1 - np.sum([c[n] / (n + 1) for n in range(1, len(c))])
+
+def Animate(nx = 8, ny = 8, res = 100, sx = 1.0, sy = 0.75, amp = 1.,
+            theta = -0.3, cx = [5/6, 1, -1], cy = [5/6, 1, -1], eps = 0.1):
   '''
   Animate the high resolution PRF with a random walk across the detector
   
   '''
+    
+  # Load the motion vectors
+  motion_vectors = np.load('data/motion_vectors.npz')
+  x0 = motion_vectors['x0'][2140:] + nx / 2.
+  y0 = motion_vectors['y0'][2140:] + ny / 2.
+  cad = np.arange(0, len(x0))
   
   # Construct the grids
-  x0 = nx / 2. - 1
-  y0 = ny / 2. - 1  
   psv = PSV(nx, ny, res, eps)
   ipv = IPV(nx, ny, res, cx, cy)
-  psf = PSF(nx, ny, res, x0, y0, sx, sy, amp, theta)
+  psf = PSF(nx, ny, res, x0[0], y0[0], sx, sy, amp, theta)
   prf = psv * ipv * psf
   flx = Pixelate(prf, nx, ny, res)
   
   # Plot
-  fig, ax = pl.subplots(1,2, figsize = (10,5))
-  hires = ax[0].imshow(prf, extent = (0, nx, 0, ny), origin = 'lower')
-  lores = ax[1].imshow(flx, extent = (0, nx, 0, ny), origin = 'lower')
-  for axis in ax:
+  fig = pl.figure(figsize = (8,8))
+  fig.subplots_adjust(top = 0.95, bottom = 0.1, left = 0.1, right = 0.95, wspace = 0.05, hspace = 0.1)
+  axhi = pl.subplot2grid((4, 2), (0, 0), colspan = 1, rowspan = 2)
+  axlo = pl.subplot2grid((4, 2), (0, 1), colspan = 1, rowspan = 2)  
+  axx0 = pl.subplot2grid((4, 2), (2, 0), colspan = 2, rowspan = 1) 
+  axy0 = pl.subplot2grid((4, 2), (3, 0), colspan = 2, rowspan = 1) 
+  hires = axhi.imshow(prf, extent = (0, nx, 0, ny), origin = 'lower', aspect = 'auto')
+  lores = axlo.imshow(flx, extent = (0, nx, 0, ny), origin = 'lower', aspect = 'auto')
+  axx0.plot(cad, x0, 'k-', alpha = 0.75, lw = 1)
+  axy0.plot(cad, y0, 'k-', alpha = 0.75, lw = 1)
+  tracker1 = axx0.axvline(0, color = 'r')
+  tracker2 = axy0.axvline(0, color = 'r')
+
+  # Appearance
+  for axis in [axhi, axlo]:
     axis.set_xticks([])
     axis.set_yticks([])
-
+  for axis in [axx0, axy0]:
+    axis.margins(0, None)
+  axx0.set_xticklabels([])
+  axx0.set_ylabel(r'$x_0$ (pixels)', fontsize = 12)
+  axy0.set_ylabel(r'$y_0$ (pixels)', fontsize = 12)
+  axy0.set_xlabel(r'Cadence', fontsize = 12)
+  
   # Animate!
-  def data_gen():
-    x = x0
-    y = y0
-    while True:
-      x += dx * np.random.randn()
-      y += dy * np.random.randn()
-      if y < 1:
-        y = 1
-      elif y > ny - 1:
-        y = ny - 1
-      if x < 1:
-        x = 1
-      elif x > nx - 1:
-        x = nx - 1
-      yield x, y
-      
-  def run(data):
-    x0, y0 = data
-    psf = PSF(nx, ny, res, x0, y0, sx, sy, amp, theta)
+  def run(i):
+    psf = PSF(nx, ny, res, x0[i], y0[i], sx, sy, amp, theta)
     prf = psv * ipv * psf
     flx = Pixelate(prf, nx, ny, res)
     hires.set_data(prf)
     lores.set_data(flx)
-    return hires, lores
+    tracker1.set_xdata([i, i])
+    tracker2.set_xdata([i, i])
+    return hires, lores, tracker1, tracker2
     
-  ani = animation.FuncAnimation(fig, run, data_gen, interval=10, repeat=False)
+  ani = animation.FuncAnimation(fig, run, frames=len(x0), interval=10, repeat=True)
   pl.show()
 
+def LnLike(p, **kwargs):
+  '''
+  TODO!
+  
+  '''
+  
+  fpix = kwargs['fpix']
+  ipv_order = kwargs['ipv_order']
+  
+  sx = p[0]
+  sy = p[1]
+  theta = p[2]
+  eps = p[3]
+  cx = np.append([0], p[4:4+ipv_order+1])
+  cy = np.append([0], p[4+1+ipv_order:])
+  
+def RunMCMC():
+  '''
+  
+  '''
+  
+  # General params
+  nx = 8
+  ny = 8
+  res = 100
+  
+  # The "true" parameters
+  cx = [5/6, 1, -1]
+  cy = [5/6, 1, -1]
+  theta = 0.3
+  sx = 0.5
+  sy = 0.75
+  eps = 0.1
+  amp = 1
+  
+  # Load the motion vectors
+  motion_vectors = np.load('data/motion_vectors.npz')
+  x0 = motion_vectors['x0'][3500:] + nx / 2.
+  y0 = motion_vectors['y0'][3500:] + ny / 2.
+  ncad = len(x0)
+  
+  # Construct the sensitivity map
+  psv = PSV(nx, ny, res, eps)
+  ipv = IPV(nx, ny, res, cx, cy)
+  
+  # Compute the pixel fluxes
+  fpix = np.empty((ncad, nx, ny))
+  for i in range(ncad):
+    psf = PSF(nx, ny, res, x0[i], y0[i], sx, sy, amp, theta)
+    prf = psv * ipv * psf
+    fpix[i] = Pixelate(prf, nx, ny, res)
+
+  # Our initial guess
+  p0 = np.concatenate([[np.abs(np.random.randn()), np.abs(np.random.randn()), 
+                        2 * np.pi * np.random.rand(), 0.1 * np.random.randn()], 
+                        np.random.randn(3), np.random.randn(3)])
+  ipv_order = len(cx) - 1
+  kwargs = dict(fpix = fpix, ipv_order = ipv_order)
+  
+  LnLike(p0, **kwargs)  
+  
+
 if __name__ == '__main__':
+  
   Animate()
